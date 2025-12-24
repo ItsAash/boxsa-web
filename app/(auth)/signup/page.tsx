d25f92f19be111118b1c95ed69f0ae3b9b32f27f
Ashish Neupane <aashishneupane125@gmail.com>
2025-12-24 23:41:03 +0545

    Remove unused SignupStep2 import from SignupPage
    Removed import of SignupStep2 component from SignupPage.

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import SignupForm from "./_components/signup-form";
-import SignupStep2 from "./_components/signup-step2";
 import { SignupProvider } from "./_context/signup-context";
 
 export default function SignupPage() {
